234d55a2ab29cedcfe355f122711d29bbe59aa1d
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-05-30 15:59:34 +0200

    bg too table

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
-<div class="container mt-5">
-    <table  class="table table-dark table-striped table-bordered">
+<div class="container mt-5 rounded-3 overflow-auto bg-warning">
+    <table  class="table table-dark table-striped table-bordered mt-3 ">
         <thead class="text-center">
             <tr>
                 <th>Title</th>
